b47592863a5c213e86baed57396a02497f8f1d2b
AleksanderDalton0511 <Aleksander.DAlton@tptlive.ee>
2023-09-01 12:23:26 +0300

    mainpage

---
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
